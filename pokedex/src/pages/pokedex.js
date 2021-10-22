2e3e83a6de34917d534f2c30aa7a6fa919e1304e
pabloapausa <pabloapausa@outlook.com>
2021-10-22 16:28:59 +0200

    Cleaner

--- pokedex/src/pages/pokedex.js
+++ pokedex/src/pages/pokedex.js
@@ -1,15 +1,15 @@
+const COUNT = 1118;
+let [limit, page] = [16, 0];
+const last = Math.floor(COUNT / limit);
+let offset;
+
 class Index {
     constructor() {
-        offset = multiply(limit, page);
+        offset = Math.floor(limit * page);
         const pokemonsPage = new PokemonsPage();
         pokemonsPage.clearView();
         pokemonsPage.setView();
     }
 }
 
-let offset;
-let [limit, page] = [16, 0];
-const multiply = (a, b) => Math.floor(a * b);
-const COUNT = 1118;
-const last = Math.floor(COUNT / limit);
 const index = new Index();
